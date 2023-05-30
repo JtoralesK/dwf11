681316f94a2716f49979d08bd870c4fc672db7ca
javier torales <jtorales2016@gmail.com>
2023-05-29 23:37:03 -0300

    se agrego sona talleres

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import { Contacto } from "./components/contacto";
-
+import { SectionTalleres } from "./components/sectionTalleres";
 export default function Home() {
   return (
     <>
+      <SectionTalleres />
       <Contacto />
     </>
   );
