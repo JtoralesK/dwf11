9001ddfd785390ef19923103f70ee83d2c336f7b
Gon <gon.mendez13@gmail.com>
2023-06-13 14:28:13 -0300

    talleres añadidos

--- src/app/components/talleres.ts
+++ src/app/components/talleres.ts
@@ -1,17 +1,17 @@
 export const ArrTalleres = [
   {
     id: 1,
-    img: "https://wordpress972997149.files.wordpress.com/2018/11/cropped-arte-musica1.jpg",
-    title: "Taller de música",
+    img: "http://3.bp.blogspot.com/-Fq2d0zFH_yM/TiOquw7WubI/AAAAAAAAAAo/fAN9mykU8J0/s748/DSC08719.JPG",
+    title: "Taller de Artes El Estudio",
     descripcion:
-      "aprende a tocar un instrumento o a cantar en nuestro taller de música. Tenemos clases individuales y grupales para todos los niveles.",
+      "DIBUJO - PINTURA - ESCULTURA - GRABADO - FILETEADO PORTEÑO - AEROGRAFIA -  PIANO - TALLA EN MADERA - TAPIZ",
   },
   {
     id: 2,
-    img: "https://wordpress972997149.files.wordpress.com/2018/11/cropped-arte-musica1.jpg",
-    title: "Taller de música",
+    img: "https://scontent.faep6-1.fna.fbcdn.net/v/t39.30808-6/346808538_620254850159298_1229470801674586750_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeFCMF2ndDmlB01Xq13qN3YPsqj9h08Tsb-yqP2HTxOxv3FHQobvt5POHluNxuegHvT8GkWEGyMGKGGtlxU4b6Bb&_nc_ohc=SEShvB4H82EAX901Q9h&_nc_ht=scontent.faep6-1.fna&oh=00_AfDr6O3W1kzclJ-oFzk_pIZdxXnvHf4P-q-g5chBIqm7bw&oe=648DABBF",
+    title: "Taller de escultura en Museo Carnacini",
     descripcion:
-      "aprende a tocar un instrumento o a cantar en nuestro taller de música. Tenemos clases individuales y grupales para todos los niveles.",
+      "Taller abierto a la comunidad, con la intención de acercar una experiencia única que es el modelado en arcilla con modelo vivo a la gente.",
   },
   {
     id: 3,
